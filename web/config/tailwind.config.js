8586968948dd5d2b0d0c59506c04b5d7d03eb0d1
augustin <azzerty23@hotmail.fr>
2022-07-02 12:26:09 +0200

    feat: setup tailwind

---
+++ web/config/tailwind.config.js
@@ -0,0 +1,20 @@
+const colors = require('tailwindcss/colors')
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: colors.orange,
+        secondary: colors.slate,
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
+}
